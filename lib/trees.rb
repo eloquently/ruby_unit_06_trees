817dcab2fdfeca4e3a9cc8941f8a1270fd2515a0
etmckay1 <ericmckay4@gmail.com>
2016-04-26 06:57:39 +0000

    initial commit

---
+++ lib/trees.rb
@@ -0,0 +1,115 @@
+class Node
+    attr_accessor :left, :right, :key, :value
+
+    def initialize(options={})
+        self.key = options[:key]
+        self.value = options[:value] || self.key
+        self.left = options[:left]
+        self.right = options[:right]
+    end
+end
+
+# print the tree's pre_order traversal
+#
+#      1
+#    /   \
+#   2     3
+#  / \    /
+# 4   5  6
+
+# should print:
+# 124536
+def pre_order(root)
+end
+
+# print the tree's in_order traversal
+#
+#      1
+#    /   \
+#   2     3
+#  / \    /
+# 4   5  6
+
+# should print:
+# 425163
+def in_order(root)
+end
+
+# print the tree's post_order traversal
+#
+#      1
+#    /   \
+#   2     3
+#  / \    /
+# 4   5  6
+
+# should print:
+# 452631
+def post_order(root)
+end
+
+# return the number of levels in the tree
+#
+#      1
+#    /   \
+#   2     3
+#  / \    /
+# 4   5  6
+#       /
+#      7
+
+# should return 3
+def height(root)
+end
+
+# print the tree's level_order traversal
+#
+#      1
+#    /   \
+#   2     3
+#  / \    /
+# 4   5  6
+
+# should print:
+# 123456
+def level_order(root)
+end
+
+# print the top view of the tree. This means you should print
+# the values of the tree you would see if you were looking down on the tree
+# from above the root node and read left to right.
+#
+#      1
+#    /   \
+#   2     3
+#  / \    /
+# 4   5  6
+#       /
+#      7
+
+# should print: 4213
+def top_view(root)
+end
+
+# find the value at the node with the specified key in the binary search tree
+# here is an example of a binary search tree (keys shown):
+#      8
+#    /   \
+#   4     15
+#  / \    /
+# 3   5  10
+
+def search_bst(root, key)
+end
+
+# insert a new key/value pair into the binary search tree
+# here is an example of inserting the key 7
+#
+#      8                8
+#    /   \            /   \
+#   4     15  -->    4    15
+#  /      /         / \   /
+# 3      10        3   7 10
+
+def insert_into_bst(root, key, value)
+end
---
+++ spec/trees_spec.rb
@@ -0,0 +1,91 @@
+require 'trees'
+
+describe Node do
+    let(:basic) do
+        root = Node.new(key: 1)
+        root.left = Node.new(key: 2)
+        root.right = Node.new(key: 3)
+        root.left.left = Node.new(key: 4)
+        root.left.right = Node.new(key: 5)
+        root.right.left = Node.new(key: 6)
+        return root
+    end
+
+    let(:tall) do
+        root = Node.new(key: 1)
+        root.left = Node.new(key: 2)
+        root.right = Node.new(key: 3)
+        root.left.left = Node.new(key: 4)
+        root.left.right = Node.new(key: 5)
+        root.right.left = Node.new(key: 6)
+        root.right.left.left = Node.new(key: 7)
+        root.right.left.left.left = Node.new(key: 8)
+        root.right.left.left.left.left = Node.new(key: 9)
+        root
+    end
+
+    let(:bst1) do
+        root = Node.new(key: 8, value: 'a')
+        root.left = Node.new(key: 4, value: 'b')
+        root.right = Node.new(key: 15, value: 'c')
+        root.left.left = Node.new(key: 3, value: 'd')
+        root.left.right = Node.new(key: 5, value: 'e')
+        root.right.left = Node.new(key: 10, value: 'f')
+        root
+    end
+
+
+    describe '#pre_order' do
+        it 'prints answer' do
+            expect { pre_order(basic) }.to output('124536').to_stdout
+        end
+    end
+
+    describe '#in_order' do
+        it 'prints answer' do
+            expect { in_order(basic) }.to output('425163').to_stdout
+        end
+    end
+
+    describe '#post_order' do
+        it 'prints answer' do
+            expect { post_order(basic) }.to output('452631').to_stdout
+        end
+    end
+
+    describe '#height' do
+        it 'returns answer' do
+            expect(height(basic)).to eq(3)
+            expect(height(tall)).to eq(6)
+        end
+    end
+
+    describe '#level_order' do
+        it 'prints answer' do
+            expect { level_order(basic) }.to output('123456').to_stdout
+        end
+    end
+
+    describe '#top_view' do
+        it 'prints answer' do
+            expect { top_view(basic) }.to output('4213').to_stdout
+        end
+    end
+
+    describe '#search_bst' do
+        it 'returns answer' do
+            expect(search_bst(bst1, 10)).to eq('f')
+            expect(search_bst(bst1, 3)).to eq('d')
+        end
+    end
+
+    describe '#insert_into_bst' do
+        it 'adds node to correct spot' do
+            expect(bst1.left.right.right).to eq(nil)
+            insert_into_bst(bst1, 7, 'new')
+            expect(bst1.left.right.right.value).to eq('new')
+            insert_into_bst(bst1, 9, 'newer')
+            expect(bst1.right.left.left.value).to eq('newer')
+        end
+    end
+end
